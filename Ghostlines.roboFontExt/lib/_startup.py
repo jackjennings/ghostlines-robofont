d7ebeec8257c1f5568339349e1a38906c887f8e7
Jack Jennings <jack@remote.gd>
2016-01-25 11:12:43 -0800

    Initial commit

---
+++ Ghostlines.roboFontExt/lib/_startup.py
@@ -0,0 +1,49 @@
+import os
+
+from AppKit import *
+from vanilla import *
+from vanilla.dialogs import message
+
+from mojo.UI import CurrentFontWindow
+from mojo.events import addObserver
+
+from lib.UI.toolbarGlyphTools import ToolbarGlyphTools
+
+class GhostlinesToolbar(object):
+    
+    base_path = os.path.dirname(__file__)
+    
+    def __init__(self):
+        addObserver(self, "addFontToolbar", "fontDidOpen")
+        addObserver(self, "addFontToolbar", "newFontDidOpen")
+       
+    def addFontToolbar(self, info):
+        window = CurrentFontWindow()
+        if window is None:
+            return
+        self.addToolbar(window, 'Deliver', 'ghostlinesUpload', 
+                        'upload.pdf', self.hiWes, index=-2)
+    
+    def addToolbar(self, window, label, identifier, filename, callback, index=-1):
+        toolbarItems = window.getToolbarItems()
+        vanillaWindow = window.window()
+        displayMode = vanillaWindow._window.toolbar().displayMode()
+        imagePath = os.path.join(self.base_path, 'ghostlines', 'resources', filename)
+        image = NSImage.alloc().initByReferencingFile_(imagePath)
+        
+        newItem = dict(itemIdentifier = identifier,
+            label = label,
+            imageObject = image,
+            callback = callback
+        )
+        
+        toolbarItems.insert(index, newItem)
+        vanillaWindow.addToolbar(toolbarIdentifier="toolbar-%s" % identifier, 
+                                 toolbarItems=toolbarItems, 
+                                 addStandardItems=False)
+        vanillaWindow._window.toolbar().setDisplayMode_(displayMode)
+    
+    def hiWes(self, sender):
+        message('Hi Wes')
+
+GhostlinesToolbar()
