4889502335dc35ffec1bd77be18b114d4c050554
Jack Jennings <jack@remote.gd>
2016-10-31 08:56:36 -0700

    Add license field to Release window

--- src/lib/ghostlines/lib_storage.py
+++ src/lib/ghostlines/lib_storage.py
@@ -5,7 +5,10 @@ class LibStorage(object):
         self.key = key
 
     def store(self, content):
-        self.lib[self.key] = content
+        if content is None and self.lib.has_key(self.key):
+            del self.lib[self.key]
+        else:
+            self.lib[self.key] = content
 
     def retrieve(self):
         if self.lib.has_key(self.key):
