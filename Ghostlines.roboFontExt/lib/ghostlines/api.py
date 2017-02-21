d96b3f5747861fa8370f51170c641d0b51af572a
Jack Jennings <jack@standard-library.com>
2017-02-21 00:17:08 -0800

    Pass RoboFont and extension version to API calls

--- Ghostlines.roboFontExt/lib/ghostlines/api.py
+++ Ghostlines.roboFontExt/lib/ghostlines/api.py
@@ -1,5 +1,8 @@
 import requests
 
+import mojo.roboFont as RF
+from mojo.extensions import ExtensionBundle
+
 from ghostlines import env
 
 
@@ -49,39 +52,39 @@ class Ghostlines(object):
         data = {'name': name, 'designer_name': designer}
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('font_family')
-        return requests.post(url, data=data, headers=headers)
+        return self.post(url, data=data, headers=headers)
 
     def font_family(self, id):
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('font_family/{}'.format(id))
-        return requests.get(url, headers=headers)
+        return self.get(url, headers=headers)
 
     def create_subscriber(self, font_family_id, name, email_address):
         data = {'font_family_id': font_family_id, 'name': name, 'email_address': email_address}
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('subscriber')
-        return requests.post(url, data=data, headers=headers)
+        return self.post(url, data=data, headers=headers)
 
     def delete_subscriber(self, id):
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('subscriber/{}'.format(id))
-        return requests.delete(url, headers=headers)
+        return self.delete(url, headers=headers)
 
     def enable_applicants_v1(self, font_family_id):
         data = {'font_family_id': font_family_id}
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('applicant/enable')
-        return requests.post(url, data=data, headers=headers)
+        return self.post(url, data=data, headers=headers)
 
     def applicant_roster(self, id):
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('applicant/{}'.format(id))
-        return requests.get(url, headers=headers)
+        return self.get(url, headers=headers)
 
     def approve_applicant(self, id):
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('applicant/{}/approve'.format(id))
-        return requests.post(url, headers=headers)
+        return self.post(url, headers=headers)
 
     def create_release(self, otfs=[], license=None, **data):
         files = [('otfs[]', o) for o in otfs]
@@ -91,7 +94,33 @@ class Ghostlines(object):
 
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         url = self.path('release')
-        return requests.post(url, data=data, headers=headers, files=files)
+        return self.post(url, data=data, headers=headers, files=files)
+
+    def get(self, url, headers=None, **kwargs):
+        if headers is None:
+            headers = {}
+
+        self.inject_version_data(headers)
+        return requests.get(url, headers=headers, **kwargs)
+
+    def post(self, url, headers=None, **kwargs):
+        if headers is None:
+            headers = {}
+
+        self.inject_version_data(headers)
+        return requests.post(url, headers=headers, **kwargs)
+
+    def delete(self, url, headers=None, **kwargs):
+        if headers is None:
+            headers = {}
+
+        self.inject_version_data(headers)
+        return requests.delete(url, headers=headers, **kwargs)
+
+    def inject_version_data(self, headers):
+        extension = ExtensionBundle("Ghostlines").version or "Beta"
+        version = "RF{};{}".format(RF.version, extension)
+        headers["X-Ghostlines-Version"] = version
 
     def path(self, endpoint):
         return '{}/{}/{}'.format(env.api_url, self.version, endpoint)
