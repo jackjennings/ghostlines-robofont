9186c094de2554bb0d88d9ce46fb1e0b156636ec
Jack Jennings <jack@standard-library.com>
2017-02-23 09:33:17 -0800

    Use SecureEditText for password entry

--- Ghostlines.roboFontExt/lib/ghostlines/windows/sign_in_window.py
+++ Ghostlines.roboFontExt/lib/ghostlines/windows/sign_in_window.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-from vanilla import Window, Button, EditText, TextBox
+from vanilla import Window, Button, EditText, TextBox, SecureEditText
 from vanilla.dialogs import message
 from defconAppKit.windows.baseWindow import BaseWindowController
 
@@ -15,11 +15,12 @@ class SignInWindow(BaseWindowController):
 
     def __init__(self, success_window):
         self.success_window = success_window
+        
         self.window.introduction = TextBox((15, 15, -15, 22), 'Sign in with your Ghostlines account.', sizeStyle='small')
         self.window.email_label = TextBox((15, 44, -15, 22), 'Email:', sizeStyle='small')
         self.window.email_field = EditText((15, 61, -15, 22))
         self.window.password_label = TextBox((15, 101, -15, 22), 'Password:', sizeStyle='small')
-        self.window.password_field = EditText((15, 119, -15, 22))
+        self.window.password_field = SecureEditText((15, 119, -15, 22))
         self.window.need_account_button = Button((15, -35, 110, 17), 'Need Account?', callback=self.make_account, sizeStyle="small")
         self.window.sign_in_button = Button((175, -38, 110, 22), 'Sign In', callback=self.sign_in)
         self.window.setDefaultButton(self.window.sign_in_button)
