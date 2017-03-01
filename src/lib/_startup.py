225c29d00411d3f123b2d65beb3d48b8d5e29044
Jack Jennings <jack@standard-library.com>
2017-03-01 01:51:42 -0800

    Improve toolbar creation

--- src/lib/_startup.py
+++ src/lib/_startup.py
@@ -2,4 +2,4 @@ from ghostlines.toolbar.manager import Manager
 from ghostlines.toolbar.release_menu_item import ReleaseMenuItem
 
 
-Manager(ReleaseMenuItem).on("fontDidOpen", "newFontDidOpen")
+Manager(ReleaseMenuItem, index=-2).on("fontDidOpen", "newFontDidOpen")
